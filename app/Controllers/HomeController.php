8d7e7dd6d92fd313782b220675e0dbddaa879f40
Muhammad Ammar <79646199+ammarzakwan-cell@users.noreply.github.com>
2024-09-30 16:03:46 +0800

    Update HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controllers;
 
-use App\Models\AttendDaily;
-use App\Models\EmpBasic;
-use RWNA\WorkingHour;
 use Psr\Http\Message\{
     ServerRequestInterface as Request,
     ResponseInterface as Response
@@ -27,71 +24,4 @@ class HomeController extends Controller
             'appName' => 'hardcoded',
         ]);
     }
-
-    public function employeeAttendDatatable(Request $request, Response $response, $args): Response
-    {
-        try {
-            $request = $request->getQueryParams();
-            $content = [];
-
-            $search = $request['search'];
-            $value = $search['value'];
-
-            $workingHour = new WorkingHour();
-            $model = EmpBasic::query();
-
-            if ($value != '') {
-                $model->with('attendDaily')->where('EmpID', 'LIKE', "%$value%");;
-            }
-            if ($request['order'][0]['column'] != 0) {
-                $columnName = $request['columns'][$request['order'][0]['column']]['data'];
-                $model->orderBy($columnName, $request['order'][0]['dir']);
-            } else {
-                $model->orderBy('id', 'DESC');
-            }
-
-            $model->offset($request['start'])->limit($request['length']);
-
-            /** @var EmpBasic $employee */
-            foreach ($model->get() as $employee) {
-                /** @var AttendDaily $attendDaily */
-                foreach ($employee->attendDaily as $attendDaily) {
-                    $normalHour = $workingHour->getNormalHour($attendDaily->getInTime1(), $attendDaily->getOutTime2());
-                    $content[] = [
-                        'emp_id' => $employee->getEmployeeID(),
-                        'last_name_2_c' => $employee->getFullName(),
-                        'date' => $attendDaily?->getDate()->format('Y-m-d'),
-                        'normal_hours' => $normalHour['hour'] . ' Hour(s) ' . $normalHour['minute'] . ' Minute(s)',
-                        'ot_hours' => $workingHour->getOvertimeHour($attendDaily->getOutTime2()) . ' Hour(s)',
-                    ];
-                }
-            }
-
-            $countModel = $model->count();
-
-            // Prepare response data
-            $responseData = [
-                'draw' => $request['draw'], // Cast draw to integer
-                'recordsTotal' => $countModel,
-                'recordsFiltered' => $countModel,
-                'data' => $content,
-            ];
-
-            // Return the response as JSON
-            $response->getBody()->write(json_encode($responseData));
-            return $response->withHeader('Content-Type', 'application/json');
-        } catch (Throwable $e) {
-            // Handle exception
-            $errorResponse = [
-                'error' => 'An error occurred: ' . $e->getMessage(),
-            ];
-
-            // Return error as JSON response
-            $response->getBody()->write(json_encode($errorResponse));
-            return $response->withHeader('Content-Type', 'application/json')
-                ->withStatus(500); // Set status to 500 (Internal Server Error)
-        }
-
-    }
-
 }
